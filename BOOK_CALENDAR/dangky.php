c3f0683acdd11617f8e71c1609a1ff06028dfa97
LeLoanHuong <leloanhuong46@gmail.com>
2020-04-18 21:06:24 +0700

    Create dangky.php

---
+++ BOOK_CALENDAR/dangky.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Đặt sân</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+     body{
+
+     }
+     .form-box{
+        width: 500px;
+        background: darkblue;
+        margin: 12% auto;
+        padding: 50px 0;
+        color: #fff;
+        box-shadow: 0 0 20px 2px rgba(0,0,0,0.5);
+    }
+    .container{
+      margin-left: 150px;
+    }
+
+    h1{
+        text-align: center;
+        font-size: 40px;
+    }
+    .container a{
+      text-decoration: none;
+      color:white;
+    }
+    .btn btn-primary btn a{
+          margin-left: 500px; 
+    }
+    .footer a{
+      padding-top: 10px;
+    }
+    h3{
+      text-align: center;
+      margin-top:-100px; 
+      text-decoration: none;
+ 
+    }
+    h3 a{
+      color: red;
+      text-decoration: none;
+
+    }
+
+   </style>
+</head>
+<body>
+
+  <div class="form-box">
+      <h1>Mời bạn chọn sân để đặt!</h1>
+      <div class="container">
+            <h2>BOOK YARD!</h2>
+            <button type="button" class="btn btn-primary btn-lg"><a href="calendar1.php">SÂN 1</a></button>
+            <button type="button" class="btn btn-primary btn-lg"><a href="calendar2.php">SÂN 2</a></button>    
+      </div>
+  </div> 
+</body>
+<footer>
+     <h3><button><a href="index.php">Back Home</a></button></h3>
+</footer>
+</html>
