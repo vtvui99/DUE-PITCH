a52553328955e387356db9b5b657c47261b294d9
LeLoanHuong <59504650+LeLoanHuong@users.noreply.github.com>
2020-04-27 22:38:55 +0700

    Update calendar2.php

--- calendar2.php
+++ calendar2.php
@@ -4,11 +4,23 @@
 <?php 
 function build_calendar($month,$year){
 	$mysqli=new mysqli('localhost','root','','datapitch');
+	
+	//Tạo mảng chứa chữ viết tên của các ngày trong tuần.
 	$daysOfWeek=array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+	
+	//Ngày đầu tiên của tháng?
 	$firstDayOfMonth=mktime(0,0,0,$month,1,$year);
+	
+	//Tháng này chứa bao nhiêu ngày?
 	$numberDays=date('t',$firstDayOfMonth);
+	
+	//Lấy thông tin về ngày đầu tiên của tháng
 	$dateComponents=getdate($firstDayOfMonth);
+	
+	//Tên của tháng?
 	$monthName=$dateComponents['month'];
+	
+	//Ngày nào trong tuần
 	$dayOfWeek=$dateComponents['wday'];
 	$datetoday=date('Y-m-d');
 
@@ -23,11 +35,13 @@ function build_calendar($month,$year){
 
 
 	$calendar.="<tr>";
-
+	
+	//Tạo tiêu đề lịch
 	foreach($daysOfWeek as $day){
 		$calendar.="<th class='header'>$day</th>";
 	}
-
+	
+	//Tạo phần còn lại của lịch
 	$currentDay=1;
 
 	$calendar.="</tr><tr>";
@@ -43,7 +57,7 @@ function build_calendar($month,$year){
 	$month=str_pad($month,2,"0",STR_PAD_LEFT);
 
 	while($currentDay<=$numberDays){
-
+		//Khi đến cột Thứ bảy thì bắt đầu 1 hàng mới
 		if($dayOfWeek==7){
 
 			$dayOfWeek=0;
@@ -79,6 +93,7 @@ function build_calendar($month,$year){
 	 $currentDay++;
 	 $dayOfWeek++;
 	}
+   //Hoàn thành hàng cuối cùng trong tháng, nếu cần thiết
   if($dayOfWeek!=7){
   	$remainingDays=7-$dayOfWeek;
   	for($i=0;$i<$remainingDays;$i++){
@@ -145,7 +160,7 @@ function checkSlots($mysqli,$date){
 
 		}
 	</style>
-	<title>Lich</title>
+	<title>Lịch</title>
 </head>
 <body>
 	<div class="container">
