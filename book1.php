a90ad0ff02e36952ba43ce6f3d68dfb7ae32b6ad
LeLoanHuong <59504650+LeLoanHuong@users.noreply.github.com>
2020-05-07 07:39:54 +0700

    Update book1.php

--- book1.php
+++ book1.php
@@ -92,9 +92,7 @@ function timeslots($duration,$cleanup,$start,$end){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Đặt lịch</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/all.css">
 </head>
@@ -251,9 +249,6 @@ function timeslots($duration,$cleanup,$start,$end){
     </footer>
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7I2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-    </script>
     <script>
     $(".book").click(function() {
         var timeslot = $(this).attr('data-timeslot');
